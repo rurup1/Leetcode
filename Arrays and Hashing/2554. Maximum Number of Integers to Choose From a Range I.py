85ff76740c555101ba9ce71937e58b4a2c2705d5
rurup1 <ruhanp070@gmail.com>
2026-06-28 12:21:50 -0400

    update

--- Arrays and Hashing/2554. Maximum Number of Integers to Choose From a Range I.py
+++ Arrays and Hashing/2554. Maximum Number of Integers to Choose From a Range I.py
@@ -1,15 +1,19 @@
-from typing import List
-
 class Solution:
     def maxCount(self, banned: List[int], n: int, maxSum: int) -> int:
-        valid = list(range(1,n+1))
+        
+        
         banned = set(banned)
+        total = 0
+        res = 0
 
-        for num in valid:
-            if num in banned:
-                valid.remove(num)
+        for i in range(1, n+1):
+            if i in banned:
+                continue
+            
+            total += i
+            if total > maxSum:
+                break
+            
+            res += 1
 
-        while sum(valid) > maxSum:
-            valid.pop()
-
-        return len(valid)
+        return res
