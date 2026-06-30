7f142996a2017ad99e477308eb657ba61f7c87e8
rurup1 <ruhanp070@gmail.com>
2026-06-30 17:32:42 -0400

    update

--- Binary Search/153. Find Minimum in Rotated Sorted Array.py
+++ Binary Search/153. Find Minimum in Rotated Sorted Array.py
@@ -10,7 +10,7 @@ class Solution:
         start, end = 0, len(nums) - 1
 
         while start < end:
-            mid = (end + start) // 2
+            mid = start + ((end - start) // 2)
             if nums[mid] < nums[end]:
                 end = mid
             else:
