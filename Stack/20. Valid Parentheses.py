9ebbcb3959e6a93cb5bc2cef517c35534b020968
rurup1 <ruhanp070@gmail.com>
2026-07-14 12:05:51 -0400

    update

---
+++ Stack/20. Valid Parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Loop through s:
+        #   if opening bracket: add to stack
+        #   if closing brakcet: check if stack.pop() is the correspondent
+
+        stack = []
+        for item in s:
+            if item in "({[":
+                stack.append(item)
+            elif stack:
+                if item == "}":
+                    if stack.pop() != "{":
+                        return False
+                elif item == ")":
+                    if stack.pop() != "(":
+                        return False
+                else:
+                    if stack.pop() != "[":
+                        return False
+            else:
+                return False
+        
+        return not stack
