9ca707ca4dfe1a8d4ffa962d8dbdff2ac234e69f
rurup1 <ruhanp070@gmail.com>
2026-07-25 10:33:01 -0400

    update

--- Stack/901. Online Stock Span.py
+++ Stack/901. Online Stock Span.py
@@ -12,3 +12,25 @@ class StockSpanner:
         self.stack.append((price, res))
         
         return res
+
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []
+        self.arr = []
+
+    def next(self, price: int) -> int:
+        res = 1
+        while self.stack and self.stack[-1] <= price:
+            self.stack.pop()
+            res += 1
+        
+        self.arr.append(price)
+        self.stack = self.arr.copy()
+
+        return res
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
