677b7af3f4b7fe9f7aa35f528e58af5213f56a34
rurup1 <ruhanp070@gmail.com>
2026-07-27 18:27:11 -0400

    update

---
+++ Stack/456. 132 Pattern.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack = []
+        second_max = -inf
+
+        for i in range(len(nums) - 1, -1, -1):
+            num = nums[i]
+            if num < second_max:
+                return True
+            
+            while stack and stack[-1] < num:
+                second_max = max(second_max, stack.pop())
+
+            stack.append(num)
+            
+        return False
