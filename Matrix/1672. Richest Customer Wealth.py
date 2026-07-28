0dfba5ee4e5a471cd2b549a1539a04f7b4b6e473
rurup1 <ruhanp070@gmail.com>
2026-07-28 12:53:55 -0400

    update'

---
+++ Matrix/1672. Richest Customer Wealth.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        res = 0
+        for account in accounts:
+            res = max(res, sum(account))
+        return res
